4df60dcd8957183d8596e6936f7875fbdb7cae59
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-28 16:29:28 +0100

    feat: add Head, Title and Navigation components

--- index.tsx
+++ index.tsx
@@ -3,6 +3,30 @@ import { renderToStaticMarkup } from "react-dom/server";
 
 const server = Bun.serve({
 	fetch: (request: Request) => {
-		return new Response("ok!");
+		return router(request.url);
 	},
 });
+
+function Head() {
+	return (
+		<>
+				<link rel="stylesheet" href="/public/index.css" />
+				<title>Hello Bun</title>
+		</>
+	)
+}
+
+function Title() {
+	return (<h1>Some Poems</h1>);
+}
+
+function Navigation() {
+	return (
+		<ul className="navigation">
+			<li><a href="/home">Home</a></li>
+			<li><a href="/poem-1">Phantom of Delight</a></li>
+			<li><a href="/poem-2">The Road Not Taken</a></li>
+			<li><a href="/poem-3">Because I could not stop for Death</a></li>
+		</ul>
+	)
+}
