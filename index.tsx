905fd1fb3f993948fbf980d78c45f9e735628a61
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-28 16:33:28 +0100

    feat: add customer router

--- index.tsx
+++ index.tsx
@@ -8,16 +8,41 @@ const server = Bun.serve({
 });
 
 
+type Page = "home" | "poem-1" | "poem-2" | "poem-3";
 
-function Home() {
+function router(url: string): Response {
+	const { pathname } = new URL(url);
+
+	switch(pathname){
+		case "/public/index.css": return new Response(Bun.file("." + pathname));
+		case "/":
+		case "/home": return toResponse(Home({ page: "home" }));
+		case "/poem-1": return toResponse(Home({ page: "poem-1" }));
+		case "/poem-2": return toResponse(Home({ page: "poem-2" }));
+		case "/poem-3": return toResponse(Home({ page: "poem-3" }));
+		default: return new Response("not found", { status: 404 });
+	}
+}
+
+function toResponse(component: JSX.Element): Response {
+	return new Response(renderToStaticMarkup(component), {
+		headers: {
+			"Content-Type": "text/html"
+		}
+	})
+}
+
+function Home({ page }: { page: Page }) {
 	return (
 		<>
 			<Head/>
 			<Title />
 			<Navigation />
+			<Content page={page}/>
 		</>
 	);
 }
+
 
 function Head() {
 	return (
@@ -41,4 +66,8 @@ function Navigation() {
 			<li><a href="/poem-3">Because I could not stop for Death</a></li>
 		</ul>
 	)
+}
+
+function Content({ page }: { page: Page }){
+	return <>{page} under construction</>
 }
